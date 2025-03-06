bddafd5865d6bee3de88184b461eb2c98ab310e8
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-03-06 06:48:45 -0300

    Configurando o restante das rotas principais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import { Login, Registration } from "./pages"
+import { Login, Registration, Home, Leads, Profile } from "./pages"
 
 function App() {
   return (
@@ -7,9 +7,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Registration />} />
-        <Route path="/home" element={<h1>Home</h1>} />
-        <Route path="/leads" element={<h1>Leads</h1>} />
-        <Route path="/perfil" element={<h1>Perfil </h1>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/leads" element={<Leads />} />
+        <Route path="/perfil" element={<Profile />} />
       </Routes>
     </Router>
   )
