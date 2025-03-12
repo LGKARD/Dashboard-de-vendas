09fe18dc322e46baa06563d9a9495fc532cf086a
Luiz Guilherme <luizguilhermefreitasneto@gmail.com>
2025-03-12 07:45:41 -0300

    Feat: Avatar List Component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,34 @@
-import { Header, CardComponent } from "@/components"
+import { Header, CardComponent, AvatarList } from "@/components"
 import { Container } from '@mui/material'
+import { currencyConverter } from "@/utils"
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1000.90)
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(2000.90)
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(3000.90)
+    },
+    
+  ]
   return (
     <>
      <Header />
      <Container maxWidth="lg">
       <CardComponent className="">Card</CardComponent>
+      <CardComponent>
+        <AvatarList listData={mockListData}/>
+      </CardComponent>
      </Container>
     </>
   )
